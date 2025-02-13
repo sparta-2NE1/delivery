d03d101d0293a156241caffd7ba7dccba07ce3e6
jjsh0208 <jjsh0208@gmail.com>
2025-02-13 17:11:22 +0900

    feat: User 엔티티 및 관련 클래스 구현
    - User 엔티티: user_id, email, password, nickname, role 필드 추가
    - UserRoles: 보안에서 사용되는 ROLE_ADMIN, ROLE_MANAGER, ROLE_USER 구현
    - 공통 레코드 로그 관리를 위한 Timestamped 추상 클래스 추가

---
+++ src/main/java/com/sparta/delivery/domain/user/entity/User.java
@@ -0,0 +1,29 @@
+package com.sparta.delivery.domain.user.entity;
+
+import com.sparta.delivery.domain.common.Timestamped;
+import com.sparta.delivery.domain.user.enums.UserRoles;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "users")
+public class User extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    private UserRoles role;
+}
