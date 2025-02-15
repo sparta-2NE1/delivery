1ac92a3be9976b0d8256dbdba9b6374d6f78d6c1
leeseowoo <leeseowoo.kr@gmail.com>
2025-02-15 14:32:52 +0900

    feat: product entity 추가

---
+++ src/main/java/com/sparta/delivery/domain/product/entity/Product.java
@@ -0,0 +1,40 @@
+package com.sparta.delivery.domain.product.entity;
+
+import com.sparta.delivery.domain.common.Timestamped;
+import com.sparta.delivery.domain.store.entity.Stores;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "p_product")
+public class Product extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Stores store;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = true)
+    private String description;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = true)
+    private int quantity;
+
+    @Column(nullable = false)
+    private boolean hidden;
+}
