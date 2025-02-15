924564ffb2aa6bffb53cf7c2fc330d7aefa3c52f
jjsh0208 <jjsh0208@gmail.com>
2025-02-15 17:04:45 +0900

    feat : Swagger UI 및 문서 경로 허용 설정 추가

--- src/main/java/com/sparta/delivery/config/SecurityConfig.java
+++ src/main/java/com/sparta/delivery/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
                 authorizationHttpRequest
                         .requestMatchers("/api/user/signup").permitAll() // 회원 가입 및 로그인은 접근 허가
                         .requestMatchers("/api/user/login").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
                 );
 
