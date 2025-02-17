55eb42512adbc320afbf79eb411b4a641aa128fa
leeseowoo <leeseowoo.kr@gmail.com>
2025-02-18 04:41:55 +0900

    feat: 예외 처리 로직 추가

--- src/main/java/com/sparta/delivery/domain/product/repository/ProductRepository.java
+++ src/main/java/com/sparta/delivery/domain/product/repository/ProductRepository.java
@@ -7,4 +7,5 @@ import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
 
+    boolean existsByNameAndStore_StoreId(String name, UUID storeId);
 }
--- src/main/java/com/sparta/delivery/domain/product/service/ProductService.java
+++ src/main/java/com/sparta/delivery/domain/product/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.sparta.delivery.domain.product.service;
 
+import com.sparta.delivery.config.global.exception.custom.DuplicateProductException;
 import com.sparta.delivery.domain.product.dto.ProductRequestDto;
 import com.sparta.delivery.domain.product.dto.ProductResponseDto;
 import com.sparta.delivery.domain.product.entity.Product;
@@ -16,9 +17,18 @@ public class ProductService {
     private final ProductRepository productRepository;
 
     public ProductResponseDto addProductToStore(UUID storeId, ProductRequestDto productRequestDto, String username) {
+        if (productRepository.existsByNameAndStore_StoreId(productRequestDto.getName(), storeId)) {
+            throw new DuplicateProductException("이미 동일한 이름의 상품이 존재합니다.");
+        }
+
         Product product = productRequestDto.toEntity(storeId);
         product.setCreatedBy(username);
-        Product savedProduct = productRepository.save(product);
-        return ProductResponseDto.from(savedProduct);
+
+        try {
+            Product savedProduct = productRepository.save(product);
+            return ProductResponseDto.from(savedProduct);
+        } catch (Exception e) {
+            throw new RuntimeException("상품 등록 중 알 수 없는 오류가 발생했습니다.");
+        }
     }
 }
