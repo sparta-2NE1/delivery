0b036924983418d3b84219c4367551f65e1efeaf
jjsh0208 <jjsh0208@gmail.com>
2025-02-17 23:47:00 +0900

    feat: PrincipalDetails role 반환 메서드 구현
    - UserRoles enum 타입으로 반환

--- src/main/java/com/sparta/delivery/config/auth/PrincipalDetails.java
+++ src/main/java/com/sparta/delivery/config/auth/PrincipalDetails.java
@@ -1,6 +1,7 @@
 package com.sparta.delivery.config.auth;
 
 import com.sparta.delivery.domain.user.entity.User;
+import com.sparta.delivery.domain.user.enums.UserRoles;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -30,5 +31,9 @@ public class PrincipalDetails implements UserDetails {
     @Override
     public String getUsername() {
         return user.getUsername();
+    }
+
+    public UserRoles getRole(){
+        return user.getRole();
     }
 }
