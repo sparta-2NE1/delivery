8c9b8ad68115558f6252cf258ba56c632b65a66b
CGYOON <ckdrmsdl9999@naver.com>
2025-02-17 12:39:21 +0900

    fix:repository 수정

--- src/main/java/com/sparta/delivery/domain/store/controller/StoreController.java
+++ src/main/java/com/sparta/delivery/domain/store/controller/StoreController.java
@@ -1,7 +1,15 @@
 package com.sparta.delivery.domain.store.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RequestMapping("/api/store")
 @RequiredArgsConstructor
 @RestController
 public class StoreController {
+
+
+
+
 }
