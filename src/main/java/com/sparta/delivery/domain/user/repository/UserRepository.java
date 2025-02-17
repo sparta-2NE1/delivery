1661ec0472cc3c24a2b2958752753aaf3450b56c
jjsh0208 <jjsh0208@gmail.com>
2025-02-17 11:51:23 +0900

    refactor: UserRepository 타입 변경
    - 변경 전 JpaRepository<User , Long>
    - 변경 후 JpaRepository<User , UUID>

--- src/main/java/com/sparta/delivery/domain/user/repository/UserRepository.java
+++ src/main/java/com/sparta/delivery/domain/user/repository/UserRepository.java
@@ -4,8 +4,9 @@ import com.sparta.delivery.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User ,Long > {
+public interface UserRepository extends JpaRepository<User , UUID> {
 
     Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
