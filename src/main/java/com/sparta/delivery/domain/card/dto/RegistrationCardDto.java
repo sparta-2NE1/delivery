0b68fc6840d7f56338267f6a72b7714e72933909
Leewon2 <thehee0407@naver.com>
2025-02-19 02:30:40 +0900

    feat : 카드 중복 등록 방지

--- src/main/java/com/sparta/delivery/domain/card/dto/RegistrationCardDto.java
+++ src/main/java/com/sparta/delivery/domain/card/dto/RegistrationCardDto.java
@@ -1,5 +1,6 @@
 package com.sparta.delivery.domain.card.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 @Builder
@@ -8,7 +9,13 @@ import lombok.*;
 @Data
 @AllArgsConstructor
 public class RegistrationCardDto {
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String cardCompany;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String cardName;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String cardNumber;
 }
