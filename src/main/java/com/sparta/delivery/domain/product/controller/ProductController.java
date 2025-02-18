82cbe546b66491ed2885a9b76b60b414f61455ba
leeseowoo <leeseowoo.kr@gmail.com>
2025-02-18 12:57:27 +0900

    feat: 상품 리스트 조회 기능 구현

--- src/main/java/com/sparta/delivery/domain/product/controller/ProductController.java
+++ src/main/java/com/sparta/delivery/domain/product/controller/ProductController.java
@@ -6,6 +6,7 @@ import com.sparta.delivery.domain.product.dto.ProductResponseDto;
 import com.sparta.delivery.domain.product.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -24,5 +25,11 @@ public class ProductController {
     public ResponseEntity<ProductResponseDto> addProductToStore(@PathVariable UUID storeId, @Valid @RequestBody ProductRequestDto productRequestDto, @AuthenticationPrincipal PrincipalDetails userDetails) {
         ProductResponseDto productResponseDto = productService.addProductToStore(storeId, productRequestDto, userDetails.getUsername());
         return ResponseEntity.status(HttpStatus.CREATED).body(productResponseDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductResponseDto>> getAllProducts(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size, @RequestParam(defaultValue = "createdAt") String sortBy, @RequestParam(defaultValue = "desc") String order) {
+        Page<ProductResponseDto> allProducts = productService.getAllProducts(page, size, sortBy, order);
+        return ResponseEntity.ok(allProducts);
     }
 }
--- src/main/java/com/sparta/delivery/domain/product/repository/ProductRepository.java
+++ src/main/java/com/sparta/delivery/domain/product/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.sparta.delivery.domain.product.repository;
 
 import com.sparta.delivery.domain.product.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
@@ -8,4 +10,6 @@ import java.util.UUID;
 public interface ProductRepository extends JpaRepository<Product, UUID> {
 
     boolean existsByNameAndStore_StoreId(String name, UUID storeId);
+
+    Page<Product> findAllByOrderByCreatedAtDescUpdatedAtDesc(Pageable pageable);
 }
