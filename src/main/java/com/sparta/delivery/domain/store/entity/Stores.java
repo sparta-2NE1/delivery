06a1087673ff59a83163d53cbac7fc0b2f7c1086
CGYOON <ckdrmsdl9999@naver.com>
2025-02-19 12:24:17 +0900

    fix:Store엔티티 연관관계 추가(User,Product,Order)

--- src/main/java/com/sparta/delivery/domain/store/entity/Stores.java
+++ src/main/java/com/sparta/delivery/domain/store/entity/Stores.java
@@ -1,7 +1,10 @@
 package com.sparta.delivery.domain.store.entity;
 import com.sparta.delivery.domain.common.Timestamped;
+import com.sparta.delivery.domain.order.entity.Order;
+import com.sparta.delivery.domain.product.entity.Product;
 import com.sparta.delivery.domain.region.entity.Region;
 import com.sparta.delivery.domain.store.enums.Category;
+import com.sparta.delivery.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -35,6 +38,15 @@ public class Stores extends Timestamped {
     @OneToMany(mappedBy = "stores")
     private List<Region> regionList = new ArrayList<>();
 
+    @OneToMany(mappedBy = "store")
+    private List<Product> productList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "stores")
+    private List<Order> orderList = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user;
 
     @Enumerated(EnumType.STRING)
     private Category category;
