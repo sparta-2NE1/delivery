9f543b4c1965525959d390e1fb98b788f8b06797
jjsh0208 <jjsh0208@gmail.com>
2025-02-20 11:46:16 +0900

    refactor : deliveryAddressServiceTest
    - 테스트 클래스 명 수정

--- src/test/java/com/sparta/delivery/deliveryAddressTest/deliveryAddressServiceTest.java
+++ src/test/java/com/sparta/delivery/deliveryAddressTest/deliveryAddressServiceTest.java
@@ -32,7 +32,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-public class deliveryAddressTest {
+public class deliveryAddressServiceTest {
 
     @InjectMocks
     private DeliveryAddressService deliveryAddressService;
