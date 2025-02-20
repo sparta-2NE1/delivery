ae8add9fcecc802037c07facf3fa8ef8551474cb
leeseowoo <leeseowoo.kr@gmail.com>
2025-02-20 10:53:58 +0900

    feat: 상품 검색 기능 구현

--- src/main/java/com/sparta/delivery/domain/product/repository/ProductRepository.java
+++ src/main/java/com/sparta/delivery/domain/product/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.sparta.delivery.domain.product.repository;
 
 import com.sparta.delivery.domain.product.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -13,4 +15,10 @@ public interface ProductRepository extends JpaRepository<Product, UUID> {
     Optional<Product> findByProductIdAndDeletedAtIsNullAndHiddenFalse(UUID productId);
 
     Optional<Product> findByProductIdAndDeletedAtIsNull(UUID productId);
+
+    Page<Product> findAllByNameContaining(String productName, Pageable pageable);
+
+    Page<Product> findAllByNameContainingAndDeletedAtIsNull(String productName, Pageable pageable);
+
+    Page<Product> findAllByNameContainingAndDeletedAtIsNullAndHiddenFalse(String productName, Pageable pageable);
 }
