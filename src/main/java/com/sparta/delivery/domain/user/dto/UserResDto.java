9f44ebd59e40d691dd7a84fb1da1a5dac8ffa84f
jjsh0208 <jjsh0208@gmail.com>
2025-02-22 18:27:16 +0900

    feat : UserService CUD 기능에 대한 AOP 기반 로깅 기능 추가
    - UserService의 생성, 수정, 삭제 메서드에 대한 메서드명, 파라미터 타입/값, 반환 값 로깅기능 구현
    - Read 기능은 로깅 대상에서 제외 (DB의 변동사항만 로깅)
    - 예외 발생시 에러메시지를 포함한 로깅 처리 추가

--- src/main/java/com/sparta/delivery/domain/user/dto/UserResDto.java
+++ src/main/java/com/sparta/delivery/domain/user/dto/UserResDto.java
@@ -3,10 +3,12 @@ package com.sparta.delivery.domain.user.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.UUID;
 
 @Getter
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserResDto {
--- src/main/java/com/sparta/delivery/domain/user/dto/UserRoleUpdateReqDto.java
+++ src/main/java/com/sparta/delivery/domain/user/dto/UserRoleUpdateReqDto.java
@@ -5,8 +5,10 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserRoleUpdateReqDto {
