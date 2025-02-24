df6a3af84ea6356349060510cadf72a23dc9735f
CGYOON <ckdrmsdl9999@naver.com>
2025-02-24 10:56:46 +0900

    운영지역 Controller에 operation어노테이션 추가

--- src/main/java/com/sparta/delivery/domain/region/controller/RegionController.java
+++ src/main/java/com/sparta/delivery/domain/region/controller/RegionController.java
@@ -4,6 +4,7 @@ import com.sparta.delivery.config.auth.PrincipalDetails;
 import com.sparta.delivery.domain.region.dto.RegionReqDto;
 import com.sparta.delivery.domain.region.dto.RegionResDto;
 import com.sparta.delivery.domain.region.service.RegionService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -27,8 +28,9 @@ public class RegionController {
 
     private final RegionService regionService;
 
+    @Operation(summary = "운영 지역 등록")
     @PostMapping("")
-    public ResponseEntity<?> // 운영 지역 등록
+    public ResponseEntity<?>
     register(@RequestBody @Valid RegionReqDto regionReqDto, BindingResult bindingResult, @AuthenticationPrincipal PrincipalDetails userDetails) {
         Map<String, Object> errorSave = new HashMap<>();
         if (bindingResult.hasErrors()) {
@@ -40,24 +42,25 @@ public class RegionController {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(regionService.regionCreate(regionReqDto, userDetails));
     }
-
-    @GetMapping("/{storeId}")//특정 가게 운영 지역 리스트 조회
+    @Operation(summary = "특정 가게 운영 지역 리스트 조회")
+    @GetMapping("/{storeId}")
     public ResponseEntity<?> regionList(@PathVariable UUID storeId, @PageableDefault(page = 0, size = 10, sort = {"createdAt", "updatedAt"}) Pageable pageable) {
         return ResponseEntity.status(HttpStatus.OK).body(regionService.getRegionList(pageable, storeId));
     }
 
-    @GetMapping("")//운영 지역 리스트 조회
+    @Operation(summary = "운영 지역 리스트 조회")
+    @GetMapping("")
     public ResponseEntity<?> regionList(@PageableDefault(page = 0, size = 10, sort = {"createdAt", "updatedAt"}) Pageable pageable) {
         return ResponseEntity.status(HttpStatus.OK).body(regionService.getAllRegionList(pageable));
     }
-
-    @GetMapping("/search")//운영 지역 검색
+    @Operation(summary = "운영 지역 검색")
+    @GetMapping("/search")
     public ResponseEntity<List<RegionResDto>> regionSearch(@RequestParam String keyword, @PageableDefault(page = 0, size = 10, sort = {"createdAt", "updatedAt"}) Pageable pageable) {
 
         return ResponseEntity.status(HttpStatus.OK).body(regionService.searchRegion(keyword, pageable));
     }
-
-    @PutMapping("/{regionId}")//운영 지역 수정
+    @Operation(summary = "운영 지역 수정")
+    @PutMapping("/{regionId}")
     public ResponseEntity<?> regionUpdate
             (@PathVariable UUID regionId, @RequestBody @Valid RegionReqDto regionReqDto, BindingResult bindingResult,
              @AuthenticationPrincipal PrincipalDetails userDetails) {
@@ -73,6 +76,7 @@ public class RegionController {
                 .body(regionService.updateRegion(regionReqDto, regionId, userDetails));
     }
 
+    @Operation(summary = "운영 지역 삭제")
     @PatchMapping("/{regionId}")//운영 지역 삭제
     public void regionDelete(@PathVariable UUID regionId, @AuthenticationPrincipal PrincipalDetails principalDetails) {
         regionService.deleteRegion(regionId, principalDetails.getUsername());
---
+++ src/test/java/com/sparta/delivery/regionTest/RegionServiceTest.java
@@ -0,0 +1,4 @@
+package com.sparta.delivery.regionTest;
+
+public class RegionServiceTest {
+}
